0f2b5c52fecaca505b6270726d2f66ae73eaddd4
zhaofei <1904409422@qq.com>
2023-07-03 14:49:10 +0800

    first commit

---
+++ login.py
@@ -0,0 +1,10 @@
+# 将图片重新命名为001.png
+# import torch
+import os
+
+import torch
+print("cuda is available: ", torch.cuda.is_available())
+
+print("hello world")
+# 输出python版本
+print("python version: " + os.sys.version)
